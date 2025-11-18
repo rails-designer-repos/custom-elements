e16e2c093e85c84926f5d9b737ac7d582399cebf
Rails Designer <devs@railsdesigner.com>
2025-11-18 17:37:14 +0700

    Optimistic UI form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,10 +8,17 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
+        sleep 1
         format.turbo_stream
       else
         format.html { redirect_to messages_path, alert: "Failed to save message" }
       end
     end
+  end
+
+  private
+
+  def message_params
+    params.expect(message: [:content])
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,4 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 import "components/click_counter"
+import "components/optimistic_form"
